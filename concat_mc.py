8be6ce4a5c6d2f646eb80432ccf77b09781707d3
Brinkslu991 <153300807+Brinkslu991@users.noreply.github.com>
2024-09-20 10:30:22 -0400

    dsdhgkj

---
+++ concat_mc.py
@@ -0,0 +1,19 @@
+#Lucas Brinks
+#9/20/24
+#Concatenation MC
+
+name = 'Lucas'
+jfood = 'Cheese Curds'
+
+line = 'My name is ' + name + ' and I love ' + jfood
+
+print(line)
+
+print("What I've learned about concatenation")
+print("Concatenation is helpful when you have to add multiple variables to a string.")
+print("Concatenation is slower comparied to f strings and it's memory intensive.")
+
+bkspicy = 'Spicy crispy chicken'
+price = str(4.49)
+
+print('At Burger King, a ' + bkspicy + 'is $' + price)
